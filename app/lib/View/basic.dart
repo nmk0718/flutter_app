8096e061a79c24703e30efa83669cd3b7f5f2c89
nmk0718 <nimingkun0718@gmail.com>
2020-12-25 11:44:54 +0800

    增加扫码功能

--- app/lib/View/basic.dart
+++ app/lib/View/basic.dart
@@ -1,26 +1,96 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class basic extends StatelessWidget {
+import 'ScanPage.dart';
+import 'login.dart';
+
+enum WhyFarther { add, delet }
+
+class basic extends StatefulWidget {
+  basicState createState() => basicState();
+}
+
+class basicState extends State<basic> {
+  String content = '';
+
+  Menu() async {
+    String result = await showMenu(
+        context: context,
+        elevation: 0.0,
+        position: RelativeRect.fromLTRB(100.0, 80.0, 0.0, 100.0),
+        color: Colors.black12,
+        items: <PopupMenuEntry<String>>[
+          PopupMenuItem(
+            value: 'Home',
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                  child: Icon(Icons.home),
+                ),
+                Text('Home'),
+              ],
+            ),
+          ),
+          PopupMenuDivider(
+            height: 1.0,
+          ),
+          PopupMenuItem(
+              value: 'scan',
+              child: Row(children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                    child: Icon(Icons.camera_alt)),
+                Text('扫一扫')
+              ])),
+          PopupMenuDivider(
+            height: 1.0,
+          ),
+          PopupMenuItem(
+              value: 'community',
+              child: Row(children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                    child: Icon(Icons.camera)),
+                Text('community')
+              ])),
+        ]);
+    switch (result) {
+      case 'Home':
+        print('点击了Home');
+        break;
+      case 'scan':
+        print('点击了scan');
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ScanPage()),
+        );
+        break;
+      case 'community':
+        print('点击了community');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('页面'),
-        elevation: 0.0,
-        actions: <Widget>[
-          PopupMenuButton(
-            icon: Icon(Icons.add),
-              itemBuilder: (BuildContext context) => [
-                PopupMenuItem(value: 'Home',child: Text('Home')),
-                PopupMenuItem(value: 'Discover',child: Text('Discover')),
-                PopupMenuItem(value: 'Community',child: Text('Community')),
-              ]
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('页面'),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Menu();
+                }),
+          ],
+        ),
+        body: Center(
+          child: Text("哈哈哈")
+        ));
   }
 }
--- app/lib/View/view.dart
+++ app/lib/View/view.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
-import '../model/post.dart';
-import 'login.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class view extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return GridViewBuilderDemo();
+    return PageViewDemo();
   }
 }
 
-class GridViewBuilderDemo extends StatelessWidget {
-  Widget _gridItemBuilder(BuildContext context, int index) {
-    return Scaffold(
-        );
-  }
+List<String> imgsa = [
+  'http://pic.netbian.com/uploads/allimg/190824/212516-15666531161ade.jpg',
+  'http://pic.netbian.com/uploads/allimg/201104/230930-1604502570e6f5.jpg',
+  'http://pic.netbian.com/uploads/allimg/200216/174956-15818465964a15.jpg',
+  'http://pic.netbian.com/uploads/allimg/200410/213246-1586525566c099.jpg',
+];
 
+List<String> imgsb = [
+  'https://alifei02.cfp.cn/creative/vcg/veer/800water/veer-315286871.jpg',
+  'https://alifei04.cfp.cn/creative/vcg/veer/800water/veer-312324043.jpg',
+  'https://alifei03.cfp.cn/creative/vcg/veer/800water/veer-350219152.jpg',
+  'https://tenfei05.cfp.cn/creative/vcg/veer/800water/veer-377531699.jpg',
+];
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      body: new Center(
-        child: new RaisedButton(
-            child: new Text('退出登录'),
-            onPressed: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                  new MaterialPageRoute(builder: (context) => login()),
-                      (route) => route == null);
-            }
-            ),
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('图片展示'),
+        elevation: 0.0,
       ),
+      body: Container(
+          child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            alignment: Alignment(0.0, 0.0),
+            child: Image.network(
+              imgsb[index],
+              fit: BoxFit.cover,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0)), //
+            ),
+          );
+        },
+        itemCount: imgsb.length,
+        viewportFraction: 0.8,
+        scale: 0.8,
+      )),
     );
   }
 }
