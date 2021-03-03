b34e2af4355d7011bb254d27ebc415f25e410c9f
nmk0718 <nimingkun@liangjian.com>
2021-03-03 10:31:46 +0800

    增加退出拦截

--- app/lib/View/home.dart
+++ app/lib/View/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'login.dart';
 import 'me.dart';
 import 'view.dart';
@@ -13,6 +14,7 @@ class home extends StatefulWidget {
 }
 
 class homeState extends State<home> {
+  DateTime _lastPressedAt;
   //当前选中的
   int _currentIndex = 0;
 
@@ -21,24 +23,44 @@ class homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: PageView(
-          controller: _pageController,
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            listview(),
-            basic(),
-            view(),
-            Me(),
-          ],
-        ),
-      ),
+      body: WillPopScope(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: PageView(
+              controller: _pageController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                listview(),
+                basic(),
+                view(),
+                Me(),
+              ],
+            ),
+          ),
+          //通过WillPopScope进行退出拦截
+          onWillPop: () async {
+            if (_lastPressedAt == null ||
+                DateTime.now().difference(_lastPressedAt) >
+                    Duration(seconds: 1)) {
+              Fluttertoast.showToast(
+                  msg: "再次返回退出应用",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIos: 1,
+                  backgroundColor: Colors.black26,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+              //两次点击间隔超过1秒，重新计时
+              _lastPressedAt = DateTime.now();
+              print(_lastPressedAt);
+              return false;
+            }
+            return true;
+          }),
       bottomNavigationBar: BottomNavigationBar(
-
         currentIndex: _currentIndex,
-        onTap: (int value){
+        onTap: (int value) {
           setState(() {
             _currentIndex = value;
             _pageController.jumpToPage(value);
@@ -51,10 +73,11 @@ class homeState extends State<home> {
         //未选中为蓝色
         unselectedItemColor: Colors.black26,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: "首页"),
-          BottomNavigationBarItem(icon: Icon(Icons.live_tv),label: "动态"),
-          BottomNavigationBarItem(icon: Icon(Icons.image_outlined),label: "图片"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "我的"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+          BottomNavigationBarItem(icon: Icon(Icons.live_tv), label: "动态"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.image_outlined), label: "图片"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "我的"),
         ],
       ),
     );
